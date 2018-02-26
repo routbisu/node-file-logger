112b4c536df014b066241b6715b439b7608835ad
Biswaranjan Rout <biswaranjan.rout@xceedance.com>
2018-02-26 15:45:47 +0530

    Fixed exception issues

--- libs/logger.js
+++ libs/logger.js
@@ -14,55 +14,60 @@ const moment = require('moment-timezone');
  **************************************************************************/
 module.exports = function(options, logLevel, errorMessage, serviceName, 
                                         methodName, errorObj, callback) {
-
-    // Log messages based on log-level
-    // If logLevel is prod or prod-trace then dont log debug/trace messages
-    if(options.logLevel.toLowerCase() === 'prod') {
-        if(logLevel) {
-            if(logLevel.toLowerCase() === 'debug' || logLevel.toLowerCase() === 'trace')
-                return;
-        }
-    }
-
-    if(options.logLevel.toLowerCase() === 'prod-trace') {
-        if(logLevel) {
-            if(logLevel.toLowerCase() === 'debug')
-                return;
-        }
-    }
-
-    // Compute filename and timestamp
-    let fileName = '';
-    if(options.dateBasedFileNaming) {
-        fileName = commonServices.GetCurrentDateFileName();
-    } else {
-        fileName = commonServices.GetLogFileName();
-    }
-
-    let time = moment().tz(options.timeZone).format(options.timeFormat);
-    let date = moment().tz(options.timeZone).format(options.dateFormat);
     
-    if(options.dateBasedFileNaming) {
-        currentTime = time;
-    } else {
-        currentTime = date + ' ' + time;
-    }
-
-    let errorLine = currentTime + ' | ' 
-                    + logLevel + ' | ' 
-                    + errorMessage + ' | '
-                    + (serviceName ? ('Service: ' + serviceName + ' | ') : '')
-                    + (methodName ? ('Method: ' + methodName + ' | ') : '')
-                    + (errorObj ? ('\n' + JSON.stringify(errorObj)) : '')
-                    + '\n';
-
-    // Log to console if needed
-    if(!options.onlyFileLogging) console.log(errorLine);
-
-    fs.appendFile(fileName, errorLine, (err) => {
-        if(err) {
-            console.log('Node file logger Error: ' + err);
+    try {
+        // Log messages based on log-level
+        // If logLevel is prod or prod-trace then dont log debug/trace messages
+        if(options.logLevel.toLowerCase() === 'prod') {
+            if(logLevel) {
+                if(logLevel.toLowerCase() === 'debug' || logLevel.toLowerCase() === 'trace')
+                    return;
+            }
         }
-        if(callback) callback(err);
-    });
+
+        if(options.logLevel.toLowerCase() === 'prod-trace') {
+            if(logLevel) {
+                if(logLevel.toLowerCase() === 'debug')
+                    return;
+            }
+        }
+
+        // Compute filename and timestamp
+        let fileName = '';
+        if(options.dateBasedFileNaming) {
+            fileName = commonServices.GetCurrentDateFileName();
+        } else {
+            fileName = commonServices.GetLogFileName();
+        }
+
+        let time = moment().tz(options.timeZone).format(options.timeFormat);
+        let date = moment().tz(options.timeZone).format(options.dateFormat);
+        
+        if(options.dateBasedFileNaming) {
+            currentTime = time;
+        } else {
+            currentTime = date + ' ' + time;
+        }
+
+        let errorLine = currentTime + ' | ' 
+                        + logLevel + ' | ' 
+                        + errorMessage + ' | '
+                        + (serviceName ? ('Service: ' + serviceName + ' | ') : '')
+                        + (methodName ? ('Method: ' + methodName + ' | ') : '')
+                        + (errorObj ? ('\n' + JSON.stringify(errorObj)) : '')
+                        + '\n';
+
+        // Log to console if needed
+        if(!options.onlyFileLogging) console.log(errorLine);
+
+        fs.appendFile(fileName, errorLine, (err) => {
+            if(err) {
+                console.log('Node file logger Error: ' + err);
+            }
+            if(callback) callback(err);
+        });
+
+    } catch(ex) {
+        
+    }
 }
